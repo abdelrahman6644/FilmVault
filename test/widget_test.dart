42d276d41794d527a872e42e52a7f53a00dba94c
Abdelrahman Khaled <112812365+abdelrahman6644@users.noreply.github.com>
2024-11-19 21:22:47 +0200

    change logo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,9 @@ import 'package:movies_app/views/HomePage.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( const MovieApp());
+    await tester.pumpWidget(const MovieApp(
+      savedThemeMode: null,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
